96d0eab32d06488aea41651797791566717280bb
Peter Booker <mail@peterbooker.com>
2018-05-01 21:55:20 +0100

    display the excerpt on archives

--- template-parts/content.php
+++ template-parts/content.php
@@ -45,18 +45,22 @@
 	<div class="entry-content grid container">
 		<div class="box small-12">
 		<?php
-		the_content( sprintf(
-			wp_kses(
-				/* translators: %s: Name of current post. Only visible to screen readers */
-				__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'pb' ),
-				array(
-					'span' => array(
-						'class' => array(),
-					),
-				)
-			),
-			get_the_title()
-		) );
+		if ( ! is_singular() ) {
+			the_excerpt();
+		} else {
+			the_content( sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'pb' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				get_the_title()
+			) );
+		}
 
 		wp_link_pages( array(
 			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'pb' ),
