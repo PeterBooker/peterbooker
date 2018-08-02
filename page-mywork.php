332efceb806b21f00f711069453994ee393452ae
Peter Booker <mail@peterbooker.com>
2018-08-03 00:59:12 +0100

    Updated mywork page

--- page-mywork.php
+++ page-mywork.php
@@ -54,6 +54,16 @@ get_header();
 
 									<div class="small-12 medium-6">
 										<div class="work">
+											<h2 class="work-title">WPDirectory <span class="label warning">Beta</span></h2>
+											<div class="tech">Go, React, Trigram Index, Regex Automata</div>
+											<p>WPDirectory is a web tool which allows lightning fast regex searches of the entire WordPress Plugin and Theme Directories. It is the natural evolution of the WPDS CLI tool, removing the need for thousands of people to locally replicate the Plugin/Theme Directories.</p>
+											<a class="button" href="https://wpdirectory.net/" target="_blank">View</a>
+											<a class="button" href="https://github.com/wpdirectory/wpdir" target="_blank">Source</a>
+										</div>
+									</div>
+
+									<div class="small-12 medium-6">
+										<div class="work">
 											<h2 class="work-title">WPDS</h2>
 											<div class="tech">Go, CLI</div>
 											<p>WordPress Directory Slurper (WPDS) is a CLI tool which allows you to maintain local copies of the WP Plugin/Theme Directories. A single executable with no dependencies makes it accessible, and highly concurrent downloads make it fast.</p>
@@ -64,18 +74,9 @@ get_header();
 
 									<div class="small-12 medium-6">
 										<div class="work">
-											<h2 class="work-title">WPDirectory <span class="label warning">WIP</span></h2>
-											<div class="tech">Vue, Go, Regex</div>
-											<p>WPDirectory will be a web service which allows regex based searching of the WordPress Plugin/Theme Directories. It is the natural evolution of the WPDS CLI tool, removing the need for thousands of people to locally replicate the Plugin/Theme Directories.</p>
-											<a class="button" href="https://github.com/wpdirectory/wpdir" target="_blank">Source</a>
-										</div>
-									</div>
-
-									<div class="small-12 medium-6">
-										<div class="work">
-											<h2 class="work-title">mtStats <span class="label warning">WIP</span></h2>
+											<h2 class="work-title">mtStats <span class="label warning">Early Prototype</span></h2>
 											<div class="tech">React, Go</div>
-											<p>mtStats is a fantasy betting and database website for MMA (Mixed Martial Arts). It seeks to encourage positive engagement in the MMA community.</p>
+											<p>mtStats is a fantasy betting and encylopedia website for MMA (Mixed Martial Arts). It seeks to encourage positive engagement in the MMA community.</p>
 											<a class="button" href="https://github.com/mtstats/mtstats" target="_blank">Source</a>
 										</div>
 									</div>
